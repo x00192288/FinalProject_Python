43d52d14b20cff7f8f263328af4025589bee4051
adamgeraghty <X00192288@myTUDublin.ie>
2023-08-07 15:45:07 +0100

    first commit

---
+++ api_connect.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def GetDublin(): #schedule this to be run every x mins and write to table in database, probably mariadb or mysql 
+    try:
+        response = requests.get("https://api.waqi.info/search/?keyword=dublin&token=404010c7adc3ea4857f096fd18785435b4da0d44")
+        print("hello")
+
+        print(response.content)
+    except:
+        raise Exception("Error returning Dublin search")
+    
+    return response.content 
+
+
+
